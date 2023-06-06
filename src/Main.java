5abf2d57bc674f8f34c2ef0dbd82746fd36dd96f
Roman Shkuruoiy <shkurupiy.roman@gmail.com>
2023-06-06 11:01:00 +0300

    a c added

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello world!");
-
-        System.out.println("Hello Java");
+        int a = 1;
+        int c = 8;
+        System.out.println(a+c);
     }
 }
